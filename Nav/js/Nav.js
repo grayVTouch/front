6e3453bfefcaa0e7ac04a9d84c5b36e5c8ac698d
running <A576236148946@126.com>
2020-06-22 11:49:36 +0800

    by cxl

--- Nav/js/Nav.js
+++ Nav/js/Nav.js
@@ -11,12 +11,21 @@
         }
         this.default = {
             click: null ,
+            // 是否选中项
+            focus: true ,
+            // 是否选中顶级项
+            topFocus: true ,
+            // 初始选中的项
+            ids: [] ,
         };
         if (!G.isValid(option)) {
             option = this.default;
         }
         this.option = {};
         this.option.click = G.isFunction(option.click) ? option.click : this.default.click;
+        this.option.focus = G.isBoolean(option.focus) ? option.focus : this.default.focus;
+        this.option.topFocus = G.isBoolean(option.topFocus) ? option.topFocus : this.default.topFocus;
+        this.option.ids = G.isArray(option.ids) ? option.ids : this.default.ids;
 
         this.run();
     }
@@ -31,14 +40,15 @@
             this.dom.firstList   = G('.list' , this.dom.nav.get(0)).first();
             this.dom.items  = G('.item' , this.dom.nav.get(0));
 
-            this.dom.lists.each(function(dom){
-                dom = G(dom);
-                if (dom.equals(self.dom.firstList.get(0))) {
+            this.dom.lists.each(function(list){
+                list = G(list);
+                if (list.equals(self.dom.firstList.get(0))) {
                     return ;
                 }
-                dom.addClass('hide');
+                list.addClass('hide');
             });
 
+            // 下拉图标初始化
             this.dom.items.each(function(item){
                 item = G(item);
                 var action  = G('.action' , item.get(0)).first();
@@ -47,6 +57,10 @@
                 if (list.children().length > 0) {
                     ico.removeClass('hide');
                 }
+            });
+
+            this.option.ids.forEach(function(id){
+                self.focusById(id);
             });
         } ,
 
@@ -75,10 +89,69 @@
             });
         } ,
 
+        hide: function() {
+            var self = this;
+            this.dom.lists.each(function(list){
+                list = G(list);
+                if (list.equals(self.dom.firstList.get(0))) {
+                    return ;
+                }
+                list.endTransition('show' , function () {
+                    list.addClass('hide');
+                });
+            });
+            this.dom.items.each(function (item) {
+                item = G(item);
+                item.removeClass('hover');
+            });
+        } ,
+
+        focusByItem: function(item){
+            this.dom.items.each(function(item){
+                item = G(item);
+                item.removeClass('cur');
+            });
+            item = G(item);
+            item.addClass('cur');
+            if (this.option.topFocus) {
+                var parents = item.parents({
+                    tagName: 'div' ,
+                    className: 'item' ,
+                } , this.dom.nav.get(0) , false , true);
+                if (parents.length < 1) {
+                    return ;
+                }
+                parents.last().addClass('cur');
+            }
+        } ,
+
+        findItemById: function(id){
+            var i = 0;
+            var item = null;
+            for (; i < this.dom.items.length; ++i)
+            {
+                item = this.dom.items.jump(i , true);
+                if (item.data('id') == id) {
+                    return item.get(0);
+                }
+            }
+            throw new Error('未找到 id 对应项');
+        } ,
+
+        focusById: function(id){
+            const item = this.findItemById(id);
+            this.focusByItem(item);
+
+        } ,
+
         clickEvent: function(e){
+            G.stop(e);
             var tar = G(e.currentTarget);
+            var id = tar.data('id');
+            this.focusById(id);
+            this.hide();
             if (G.isFunction(this.option.click)) {
-                G.invoke(this.option.click , this , tar.get(0));
+                G.invoke(this.option.click , this , id , tar.get(0));
             }
         } ,
 
